e6da0b98b86984b4220bf9cba2ee63fa560a39c0
anhlh <anhlh@demetio.com>
2025-11-12 07:00:43 +0700

    Fix: body shell order detail

--- src/pages/users/OrderDetailUI/BodyShellOrderDetail.tsx
+++ src/pages/users/OrderDetailUI/BodyShellOrderDetail.tsx
@@ -2,6 +2,41 @@ import {country, insuranceType, optionDate, optionDateNoTime, orderStatus, vehic
 import {Link} from "react-router-dom";
 import {InfoSection} from "@/pages/users/OrderDetailUI/template";
 
+export const CarTypeList = [
+  { value: '01', label: 'Xe con chở người dưới 9 chỗ' },
+  { value: '02', label: 'Xe con chở người 9 chỗ' },
+  { value: '03', label: 'Xe con chở người trên 9 chỗ' },
+  { value: '04', label: 'Xe Taxi công nghệ' },
+  { value: '05', label: 'Xe taxi' },
+  { value: '06', label: 'Xe pickup' },
+  { value: '07', label: 'Xe tải dưới 3 tấn' },
+  { value: '08', label: 'Xe tải từ 3 tấn đến 8 tấn' },
+  { value: '09', label: 'Xe tải từ 8 tấn đến 10 tấn' },
+  { value: '10', label: 'Xe tải từ 10 tấn đến 15 tấn' },
+  { value: '11', label: 'Xe tải trên 15 tấn' },
+  { value: '12', label: 'Xe đầu kéo' },
+  { value: '13', label: 'Xe đông lạnh nhỏ hơn hoặc bằng 3,5 tấn' },
+  { value: '14', label: 'Xe đông lạnh trên 3,5 tấn' },
+  { value: '15', label: 'Xe romooc ben (Tự đổ)' },
+  { value: '16', label: 'Romooc' },
+  { value: '17', label: 'Xe bảo ôn đến 3,5 tắn' },
+  { value: '18', label: 'Xe bảo ổn 3,5 tấn' },
+  { value: '19', label: 'Xe van' },
+  { value: '20', label: 'Xe bus nội tỉnh' },
+  { value: '21', label: 'Xe bus liên tỉnh' },
+  { value: '22', label: 'Xe khách liên tỉnh' },
+] as const;
+
+export const CarIntendedUseConst = {
+  BUSINESS: '01',
+  PERSONAL: '02',
+} as const;
+
+export const CarIntendedUse = [
+  { value: CarIntendedUseConst.BUSINESS, label: 'Xe kinh doanh' },
+  { value: CarIntendedUseConst.PERSONAL, label: 'Xe không kinh doanh' },
+] as const;
+
 export default function BodyShellOrderDetail ({ data }: { data: any }) {
 
   const buyerInfo = [
@@ -12,15 +47,16 @@ export default function BodyShellOrderDetail ({ data }: { data: any }) {
   ];
 
   const carInfo = [
-    { label: 'Biển kiểm soát', value: data.insuranceOrder?.licensePlate },
+    { label: 'Biển kiểm soát', value: data.insuranceOrder?.licensePlate || 'Chưa có' },
     { label: 'Số khung', value: data.insuranceOrder?.chassisNumber },
     { label: 'Số máy', value: data.insuranceOrder?.engineNumber },
     { label: 'Hãng xe', value: data.insuranceOrder?.carCompany },
     { label: 'Hiệu xe', value: data.insuranceOrder?.vehicleModel },
-    { label: 'Loại xe', value: data.insuranceOrder?.vehicleType },
+    { label: 'Giá xe khai báo', value: `₫${data.insuranceOrder?.carPrice.toLocaleString('vi-VN')}` },
+    { label: 'Loại xe', value: CarTypeList.find(item => item.value === data.insuranceOrder?.vehicleType)?.label || '' },
     { label: 'Năm sản xuất', value: data.insuranceOrder?.yearOfManufacture },
-    { label: 'Số chỗ ngồi', value: data.insuranceOrder?.numberOfSeat },
-    { label: 'Mục đích sử dụng', value: `${data.insuranceOrder?.isBusinessVehicle ? 'Xe kinh doanh' : 'Xe không kinh doanh'}` },
+    { label: 'Số chỗ ngồi', value: data.insuranceOrder?.numberOfSeat + ' Chỗ' },
+    { label: 'Mục đích sử dụng', value: CarIntendedUse.find(item => item.value === data.insuranceOrder?.intendedUse)?.label || '' },
   ]
 
   const insuranceInfo = [
